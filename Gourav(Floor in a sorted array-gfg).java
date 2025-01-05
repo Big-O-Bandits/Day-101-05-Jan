a0ed75fc83ca05ce82938b8389d3c5de34545fe3
Gourav Kumar <137706422+Gouravkumar08@users.noreply.github.com>
2025-01-05 14:38:43 +0530

    Update Gourav(Floor in a sorted array-gfg).java

--- Gourav(Floor in a sorted array-gfg).java
+++ Gourav(Floor in a sorted array-gfg).java
@@ -1,43 +1,21 @@
 class Solution {
-    public int[] getFloorAndCeil(int x, int[] arr) {
-        // code here
-        Arrays.sort(arr);
-        int[]res=new int[2];
-        res[0]=getfloor(arr,x);
-        res[1]=getceil(arr,x);
-        return res;
-        
-    }
-    private int getfloor(int[]arr,int x){
+
+    static int findFloor(int[] arr, int k) {
+        // write code here
         int low=0;
         int high=arr.length-1;
         int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(arr[mid]<=x){
-                ans=arr[mid];
+            if(arr[mid]<=k){
+                ans=mid;
                 low=mid+1;
-            }else if(arr[mid]>x){
+                
+            }else if(arr[mid]>k){
                 high=mid-1;
             }
         }
         return ans;
         
-    }
-    private int getceil(int[] arr, int x){
-        
-        int low=0;
-        int high=arr.length-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(arr[mid]>=x){
-                ans=arr[mid];
-                high=mid-1;
-            }else if(arr[mid]<x){
-                low=mid+1;
-            }
-        }
-        return ans;
     }
 }
